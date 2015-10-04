e29e40af9886cda95e5520fe90fe54d877137bab
ams <sullivan1513@gmail.com>
2015-10-03 20:53:52 -0400

    Problem 13 solved

---
+++ src/problems_10_19/Problem13.java
@@ -0,0 +1,41 @@
+package problems_10_19;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Problem13 {
+	
+	public static void main(String[] args) throws IOException
+	{
+		FileReader fileReader = new FileReader("/home/ams/Dropbox/workspace/Project_Euler/src/Problem13_Numbers.txt");
+
+		
+		BufferedReader lineReader = new BufferedReader(fileReader);
+		
+		int numberOfLines = 100;
+		
+		BigInteger[] numberString = new BigInteger[numberOfLines];
+		
+		for(int i = 0; i<numberOfLines; i++)
+		{
+//			System.out.println(lineReader.readLine());
+			numberString[i] = new BigInteger(lineReader.readLine());
+			
+		}
+		
+		
+		System.out.println(Arrays.deepToString(numberString));
+		
+		BigInteger bigSum = BigInteger.ZERO;
+		
+		for(BigInteger num : numberString)
+		{
+			bigSum = bigSum.add(num);
+		}
+		
+		System.out.println(bigSum + " : " + bigSum.toString().substring(0,10));
+		
+	}
+	
+}
